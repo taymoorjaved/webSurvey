e03b5de86f085494ec67b4e32ffac2657672979a
Taymoor Javed <taymoorjaved320@gmail.com>
2022-08-01 04:08:19 +0200

    add submit form logic

--- src/components/surveySteps/surveySteps/StepThree.tsx
+++ src/components/surveySteps/surveySteps/StepThree.tsx
@@ -1,9 +1,9 @@
-import {FormikContext} from 'formik';
-import React from 'react';
-import {Button} from '../../common/button/Button';
-import {Spacer} from '../../common/spacer/Spacer';
-import {Text} from '../../common/text/Text';
-import {QuestionThree} from '../SurveyConfig';
+import { FormikContext } from "formik";
+import React from "react";
+import { Button } from "../../common/button/Button";
+import { Spacer } from "../../common/spacer/Spacer";
+import { Text } from "../../common/text/Text";
+import { QuestionThree } from "../SurveyConfig";
 
 interface P {
   onNext: () => void;
@@ -13,23 +13,30 @@ interface P {
 // StepThree component used in survey wrapper
 const StepThree = (props: P) => {
   const form = React.useContext(FormikContext);
+
+  // handle form submit
+  const submitForm = () => {
+    form.submitForm();
+    props.onNext();
+  };
+
   return (
     <React.Fragment>
-      <Text variant='h1' fontSize={20} weight='bold'>
+      <Text variant="h1" fontSize={20} weight="bold">
         QUESTION 3
       </Text>
-      <Text variant='p' fontSize={48} weight='bold'>
+      <Text variant="p" fontSize={48} weight="bold">
         Where are you from?
       </Text>
       <div>
         <select
-          name='questionThree'
-          className='customSelector'
+          name="questionThree"
+          className="customSelector"
           onChange={(e: React.ChangeEvent<any>) => {
-            form.setFieldValue('questionThree', QuestionThree[e.target.value]);
+            form.setFieldValue("questionThree", QuestionThree[e.target.value]);
           }}
         >
-          <option value='' hidden>
+          <option value="" hidden>
             Please chose your country?
           </option>
           {QuestionThree.map((q, i) => (
@@ -39,10 +46,10 @@ const StepThree = (props: P) => {
           ))}
         </select>
       </div>
-      <Spacer margin='68px 0' />
+      <Spacer margin="68px 0" />
       <Button
         withIcon
-        onClick={props.onNext}
+        onClick={submitForm}
         disabled={!form.values.questionThree}
       >
         FINISH
@@ -54,4 +61,4 @@ const StepThree = (props: P) => {
   );
 };
 
-export {StepThree};
+export { StepThree };
